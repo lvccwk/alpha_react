427277b4f0881f142610e758203c007063c8bea4
Anthony <antonyeung919@gmail.com>
2023-03-30 12:53:37 +0800

    edit

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,7 +1,7 @@
 import { IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonLabel, IonMenu, IonMenuButton, IonPage, IonRefresher, IonRefresherContent, IonRow, IonSearchbar, IonSegment, IonSegmentButton, IonTitle, IonToolbar } from '@ionic/react';
 import { ellipsisHorizontal, ellipsisVertical, personCircle, search } from 'ionicons/icons';
 import React from 'react';
-import { Route, useParams } from 'react-router';
+import { Route, useHistory, useParams } from 'react-router';
 import Card from '../components/Card';
 import ProductDetail from '../components/ProductDetail';
 import TeacherDetail from '../components/TeacherDetail';
