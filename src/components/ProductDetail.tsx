cfc77b166706f119f9f5ad7289d063c81ef4b97a
Lawrence <lawrence_@live.hk>
2023-04-06 14:34:49 +0800

    delete cart item

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { IonAvatar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem, IonLabel } from '@ionic/react';
 import { fetchProduct, fetchTeacher, fetchTeacherAll, } from '../api/fetchAll';
 import { useQuery } from '@tanstack/react-query';
-import AddToCartBtn from './AddToCartBtn';
 import photo from '../../src/photo/brandi-redd-6H9H-tYPUQQ-unsplash.jpg'
 import { useHistory, useParams } from 'react-router-dom';
 import './TeacherDetail.css';
@@ -20,10 +19,10 @@ function ProductDetail() {
             <IonCard>
                 <img alt="Silhouette of mountains" src={photo} />
                 <IonCardContent>老師:{data?.teacher.user.username}
-                <br/>
-                {data?.name} 
-                <br/>
-                評分:{data?.avg_rating}<AddToCartBtn /></IonCardContent>
+                    <br />
+                    {data?.name}
+                    <br />
+                    評分:{data?.avg_rating} </IonCardContent>
             </IonCard>
         </>
     );
