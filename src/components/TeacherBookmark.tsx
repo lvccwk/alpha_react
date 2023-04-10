68878539fa451a7b432ab45db3c56d21fe2345ed
freyy9 <0fr3ddy0@gmail.com>
2023-04-10 22:40:14 +0800

    teacher_bookmark

--- src/components/TeacherBookmark.tsx
+++ src/components/TeacherBookmark.tsx
@@ -1,15 +1,38 @@
 import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { useDispatch } from "react-redux";
+import { useAppSelector } from "../redux/store";
 import { IonButton, IonIcon, IonToast } from '@ionic/react';
 import { bookmark, bookmarkOutline } from 'ionicons/icons';
+import { fetchTeacher, fetchCreateBookmark } from '../api/fetchAll';
+
+
 
 function TeacherBookmark() {
     const [isBookmarked, setIsBookmarked] = useState(false);
     const [showToast, setShowToast] = useState(false);
 
+    const params = useParams()
+    const teacherId = Object.values(params)[0]
+    const { data, isLoading, error, refetch } = useQuery({
+        queryKey: ["teacherDetail"],
+        queryFn: () => fetchTeacher(Number(teacherId)),
+    });
 
+    const dispatch = useDispatch();
+    const studentId = useAppSelector(state => state.user.id)
     const handleClick = () => {
         setIsBookmarked(!isBookmarked);
+        const obj = {
+            user_id: Number(studentId),
+            teacher_id: Number(teacherId)
+        }
+        if (!isBookmarked) {
+            fetchCreateBookmark(obj);
+        }
         setShowToast(true);
+        console.log(obj.user_id, obj.teacher_id);
     };
     const toastMessage = isBookmarked ? 'Teacher bookmarked' : 'Bookmark removed';
 
