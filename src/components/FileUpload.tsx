e9b7696ef4d57ee6094185fe86c51365fdd3d1b9
freyy9 <0fr3ddy0@gmail.com>
2023-04-12 12:40:59 +0800

    upload_product

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -24,7 +24,7 @@ import { useMutation, useQuery } from "@tanstack/react-query";
 
 function FileUpload() {
   const [name, setName] = useState<string>("");
-  const [price, setPrice] = useState<number>(0);
+  const [price, setPrice] = useState<number|null>(null);
   const [product, setProduct] = useState<string>("");
   const [subject, setSubject] = useState<string>("");
 
@@ -58,8 +58,24 @@ function FileUpload() {
     <IonCard>
       <IonCardContent>
         <form onSubmit={onSubmit}>
-          <IonInput placeholder="Name" /><br />
-          <IonInput placeholder="Price ($ HKD)"/><br />
+
+          課程/筆記名稱
+          <IonInput aria-label="Custom input"
+            value={name} 
+            placeholder="Name"
+            onIonChange={(e: any) => { console.log(e); setName(e.target.value) }}>
+            
+          </IonInput><br />
+
+          價錢
+          <IonInput aria-label="Custom input"
+            value={price} 
+            placeholder="Price ($ HKD)"
+            onIonChange={(e: any) => { console.log(e); setPrice(e.target.value) }}>
+            
+          </IonInput><br />
+          
+          選擇課程/筆記
           <IonSelect
             value={product}
             placeholder="Select Product Type"
@@ -68,6 +84,24 @@ function FileUpload() {
             <IonSelectOption value="notes">筆記</IonSelectOption>
           </IonSelect><br />
           
+          選擇科目
+          <IonSelect
+            value={subject}
+            placeholder="Select Subject"
+            onIonChange={(e: any) => setSubject(e.detail.value)} >
+            <IonSelectOption value="Chinese">中文</IonSelectOption>
+            <IonSelectOption value="English">英文</IonSelectOption>
+            <IonSelectOption value="Mathematics">數學</IonSelectOption>
+            <IonSelectOption value="Economics">經濟</IonSelectOption>
+            <IonSelectOption value="Liberal Studies">通識</IonSelectOption>
+            <IonSelectOption value="Biology">生物</IonSelectOption>
+            <IonSelectOption value="Chemistry">化學</IonSelectOption>
+            <IonSelectOption value="Physics">物理</IonSelectOption>
+            <IonSelectOption value="Geography">地理</IonSelectOption>
+            <IonSelectOption value="History">歷史</IonSelectOption>
+            <IonSelectOption value="Chinese History">中國歷史</IonSelectOption>
+          </IonSelect><br />
+
           <input type="file" onChange={handleCreateProduct} />
           <br />
           <button type="submit">UPLOAD</button>
