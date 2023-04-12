f55fdb953df5fe5b40b31ccc6648158d51b7633b
Anthony <antonyeung919@gmail.com>
2023-04-12 18:07:45 +0800

    edit

--- src/pages/ShoppingCart.tsx
+++ src/pages/ShoppingCart.tsx
@@ -1,24 +1,31 @@
-import { IonButton, IonCardTitle, IonContent, IonPage, } from '@ionic/react';
+import { IonButton, IonCardTitle, IonContent, IonPage } from '@ionic/react';
 import React from 'react';
 import Button from '../components/Button';
 import CartItem from '../components/CartItem';
 import ToolBar from '../components/Toolbar';
+import { useAppSelector } from '../redux/store';
 
 
 const ShoppingCart: React.FC = () => {
+    const isLoggedIn = useAppSelector(state => state.user.isLoggedIn)
+    
+
 
     return (
         <>
             <IonPage>
                 <ToolBar />
+                {isLoggedIn === false && (
+                    <IonContent>
+                        你尚未登入
+                    </IonContent>
+                )}
+                {isLoggedIn === true && (
                 <IonContent>
                     <IonCardTitle>購物車</IonCardTitle>
                     <CartItem />
-                    <IonButton>
-                        前往付款
-                    </IonButton>
-                    <Button />
                 </IonContent>
+                )}
             </IonPage>
         </>
     );
