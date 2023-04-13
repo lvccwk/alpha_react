19b9884b56830e76d86c1e7dfb6104c7bcbdaf84
Lawrence <lawrence_@live.hk>
2023-04-13 17:10:12 +0800

    chatroom isLoggedIn

--- src/components/UiDesign/ChatroomContact.tsx
+++ src/components/UiDesign/ChatroomContact.tsx
@@ -6,10 +6,11 @@ import { useQuery } from '@tanstack/react-query';
 import { fetchTeacherAll, fetchUserAll, } from '../../api/fetchAll';
 import { FollowedTeacherInterface, ProductInterface, TeacherSubjectInterface, UserInterface } from '../../interface/interface';
 import { fetchUser } from '../../api/fetchUser';
+import { useAppSelector } from '../../redux/store';
 
 
 function ChatroomContact() {
-
+    const isLoggedIn = useAppSelector(state => state.user.isLoggedIn)
     const params = useParams()
     const user_id = (Number(Object.values(params)[0]))
     const { data: userAll, isLoading, error, refetch } = useQuery({
@@ -22,24 +23,33 @@ function ChatroomContact() {
         history.push(`/chatroom/` + id);
     }
 
-    return (
-        <>
-            {Array.isArray(userAll) && userAll.map((userAll: UserInterface) => (
-                <IonItem button
-                    className='chatPeopleButton ion-button'
-                    key={userAll.id}
-                    onClick={() => onClickChatBox(userAll.id)}>
-                    <IonAvatar className='chatPeople' slot="start">
-                        <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
-                    </IonAvatar>
-                    <IonLabel>
-                        {userAll.username}
-                    </IonLabel>
 
-                </IonItem>
+    if (isLoggedIn === false) {
+        return (
+            <div>尚未登入</div>
+        )
+    } else {
 
-            ))}
-        </>
-    );
+        return (
+            <>
+                {Array.isArray(userAll) && userAll.map((userAll: UserInterface) => (
+                    <IonItem button
+                        className='chatPeopleButton ion-button'
+                        key={userAll.id}
+                        onClick={() => onClickChatBox(userAll.id)}>
+                        <IonAvatar className='chatPeople' slot="start">
+                            <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+                        </IonAvatar>
+                        <IonLabel>
+                            {userAll.username}
+                        </IonLabel>
+
+                    </IonItem>
+
+                ))}
+            </>
+        );
+    }
 }
+
 export default ChatroomContact;
